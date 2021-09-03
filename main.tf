75c5784162ba841bb4af5695e4c65d3a8af962cd
Jy Kingston <jy.kingston@hashicorp.com>
2021-09-03 15:40:30 +1000

    Fix module

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
+  source  = "app.terraform.io/jykingston-hashicorp/s3-webapp/aws"
+  version = "1.0.1"
+  name   = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
 }
